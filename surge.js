02e96a925938bceea80ed482d0a9d05aeddba14f
oliver-cameron <94218640+oliver-cameron@users.noreply.github.com>
2023-04-28 18:00:21 +1000

    Keyboard added

--- surge.js
+++ surge.js
@@ -1,9 +1,15 @@
-var keydown = ""
+var key = {}
+for(i = 97; i < 122; i++) {
+    key.fromCharCode(i) = false;
+}
+document.addEventListener("keydown", function(e) {
+    key[e.key] = true;
+});
+document.addEventListener("keyup", function(e) {
+    key[e.key] = false;
+})
 var canvas = document.getElementById("gameplay"); 
 var ctx = canvas.getContext("2d");
 setInterval(() => {
     
 }, 50);
-document.addEventListener('keydown', (event) => {
-    keydown = event.key
-});
